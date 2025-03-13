f9fee10939226a67de1910ba57a43f600b9bd3ac
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-03-14 01:08:49 +0800

    `feat`: navbar partials

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar border-bottom">
+    <div class="container-fluid">
+        <a class="navbar-brand fw-bold text-light" href="#">BOOKING.PS</a>
+        <span class="right-navbar d-flex flex-row justify-content-evenly">
+            <button class="btn btn-light btn-sm me-2">Masuk</button>
+            <button class="btn btn-light btn-sm">Daftar</button>
+        </span>
+    </div>
+</nav>
