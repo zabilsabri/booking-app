486dda103cf2044bddbc19fa66fec3fe08947304
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-03-14 01:09:03 +0800

    `feat`: footer partials

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="{{ asset('css/partials/footer.css') }}">
+<footer class="pb-4 bg-white">
+    <hr style="display: block !important;">
+    <div class="footer container d-flex justify-content-between align-items-center mt-4">
+        <div class="left-footer font-smaller">
+            <p class="fw-bold mb-1">Contact Us:</p>
+            <p class="m-0">whatsapp: 0816-280-445</p>
+            <p class="m-0">Email: zabilsabrimuhammad0@gmail.com</p>
+        </div>
+        <div class="right-footer">
+            <div class="text-center m-0">
+                <h5>BOOKING.PS</h5>
+            </div>
+            <div class="footer-account mt-1 text-center mb-1">
+                <a href="https://www.instagram.com/zabilsabri/" class="footer-account-detail"><i class="fi fi-brands-instagram"></i></a>
+                <a href="https://www.wa.link/dh48s8" class="footer-account-detail"><i class="fi fi-brands-whatsapp"></i></a>
+            </div>
+            <div class="wm">
+                <small class="text-secondary font-smaller">2025 Sodra Tech | BOOKING.PS</small>
+            </div>
+        </div>
+    </div>
+</footer>
