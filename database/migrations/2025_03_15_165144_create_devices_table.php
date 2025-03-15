bb64b3b0569b27bac0e5a6b533a30acf74630967
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-03-16 01:39:26 +0800

    `feat`: devices table

---
+++ database/migrations/2025_03_15_165144_create_devices_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('devices', function (Blueprint $table) {
+            $table->uuid('id')->primary()->default(DB::raw('(UUID())'));
+            $table->string('name');
+            $table->unsignedInteger('price');
+            $table->text('description');
+            $table->unsignedInteger('additional_price')->nullable();
+            $table->string('additional_price_description')->nullable();
+            $table->string('image');
+            $table->string('status');
+            $table->string('joysticks_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('devices');
+    }
+};
