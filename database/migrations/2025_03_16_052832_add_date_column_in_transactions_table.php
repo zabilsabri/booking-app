89d83509c6aba19ea0e57bb19167ee9e5fc2fb23
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-03-16 19:59:36 +0800

    `feat`: update table transactions

---
+++ database/migrations/2025_03_16_052832_add_date_column_in_transactions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->date('book_date')->after('snap_token');
+            $table->unsignedInteger('sessions')->after('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('book_date');
+            $table->dropColumn('sessions');
+        });
+    }
+};
