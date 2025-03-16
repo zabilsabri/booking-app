de694d12807a0ba85f1275f28341840404cb0083
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-03-16 20:09:17 +0800

    `feat`: add booking list page

---
+++ app/Http/Controllers/Booking/BookingController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers\Booking;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Device;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
+use App\Models\Transaction;
+
+class BookingController extends Controller
+{
+    public function index()
+    {
+        $transactions = Transaction::with('device', 'user')->where('user_id', Auth::user()->id)->paginate(10);
+        return view('booking.index', compact('transactions'));
+    }
+
+    public function bookProcess(Request $request) {
+        $request->validate([
+            'device_id' => 'required',
+            'book_date' => 'required',
+            'sessions' => 'required',
+        ]);
+
+        $device = Device::find($request->device_id);
+
+        $book_date = Carbon::parse($request->book_date);
+        $total_price = $device->getRawOriginal('price') * $request->sessions;
+
+        if($book_date->isSaturday() || $book_date->isSunday()) {
+            $total_price = $total_price + $device->getRawOriginal('additional_price');
+        }
+
+        // Set your Merchant Server Key
+        \Midtrans\Config::$serverKey = config('midtrans.server_key');
+        // Set to Development/Sandbox Environment (default). Set to true for Production Environment (accept real transaction).
+        \Midtrans\Config::$isProduction = false;
+        // Set sanitization on (default)
+        \Midtrans\Config::$isSanitized = true;
+        // Set 3DS transaction for credit card to true
+        \Midtrans\Config::$is3ds = true;
+
+        $order_id = rand();
+
+        $params = array(
+            'transaction_details' => array(
+                'order_id' => $order_id,
+                'gross_amount' => $total_price,
+            ),
+            'customer_details' => array(
+                'first_name' => Auth::user()->name,
+                'email' => Auth::user()->email,
+            ),
+        );
+
+        $snapToken = \Midtrans\Snap::getSnapToken($params);
+
+        $uuid = (string) Str::uuid();
+        $transaction = DB::table('transactions')->insert([
+            'id' => $uuid,
+            'order_id' => $order_id,
+            'device_id' => $request->device_id,
+            'user_id' => Auth::user()->id,
+            'book_date' => $request->book_date,
+            'sessions' => $request->sessions,
+            'total_price' => $total_price,
+            'snap_token' => $snapToken,
+            'status' => 'pending',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+
+        return redirect()->route('detail', $uuid);
+    }
+}
---
+++ resources/views/booking/index.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app')
+@section('title', 'Booking')
+
+@section('content')
+<table class="table table-bordered text-center">
+  <thead>
+    <tr>
+      <th style="width: 50px" class="text-center" scope="col">#</th>
+      <th scope="col">Order ID</th>
+      <th scope="col">Device</th>
+      <th scope="col">Session</th>
+      <th scope="col">Total Price</th>
+      <th scope="col">Status</th>
+      <th scope="col">Detail</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($transactions as $index => $booking)
+    <tr>
+        <th class="text-center" scope="row">{{ $loop->iteration + ($transactions->firstItem() - 1) }}</th>
+        <td>{{ $booking -> order_id }}</td>
+        <td>{{ $booking -> device -> name }}</td>
+        <td>{{ $booking -> sessions }}</td>
+        <td>Rp{{ $booking -> total_price }}</td>
+        <td><span class="badge text-bg-{{ $booking -> status != 'completed' ? 'warning' : 'success' }}">{{ $booking -> status }}</span></td>
+        <td><a href="{{ route('detail', $booking -> id) }}" class="btn btn-primary">Detail</a></td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
+<div class="mt-4 d-flex justify-content-end">
+  {{ $transactions->links() }}
+</div>
+@endsection
