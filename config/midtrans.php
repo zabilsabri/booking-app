b504748758cbc50b9cdcda1056bfab7ab00012f8
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-03-16 20:07:15 +0800

    `feat`: add midtrans configuration

---
+++ config/midtrans.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'is_production' => env('MIDTRANS_IS_PRODUCTION'),
+    'server_key' => env('MIDTRANS_SERVER_KEY'),
+    'client_key' => env('MIDTRANS_CLIENT_KEY'),
+    'is_sanitized' => env('MIDTRANS_IS_SANITIZED'),
+    'is_3ds' => env('MIDTRANS_IS_3DS'),
+];
